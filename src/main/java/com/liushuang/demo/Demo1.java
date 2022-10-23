0e32369dbc5f96cc5238c5ffafdf993cd49cce5b
柳爽 <ls822318@163.com>
2022-10-23 20:09:38 +0800

    第一次提交

---
+++ src/main/java/com/liushuang/demo/Demo1.java
@@ -0,0 +1,11 @@
+package com.liushuang.demo;
+
+/**
+ * 集成git后续测试使用
+ * add：添加到暂存区
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello，what is your name？");
+    }
+}
