c07161243b91812c914f7967069be579a4c85ea7
柳爽 <ls822318@163.com>
2022-10-23 20:27:51 +0800

    添加了注释

--- src/main/java/com/liushuang/demo/Demo2.java
+++ src/main/java/com/liushuang/demo/Demo2.java
@@ -1,5 +1,8 @@
 package com.liushuang.demo;
 
+/**
+ * 测试本地库的版本回退模式，分别有soft，mix，hard等
+ */
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("i have a dog");
